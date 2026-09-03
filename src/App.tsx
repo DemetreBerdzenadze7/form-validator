d473f59e9eb16bb5b898a4a8187cc8b74a683a9c
Demetre Berdzenadze <berdzena.demetre@gmail.com>
2026-09-03 22:02:27 +0400

    feat: implement Header component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Header from "./components/header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="w-screen h-screen bg-error px-8 py-17 bg-[url(/images/bg-intro-mobile.png)]">
+        <div>
+          <Header />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="text-center">
+      <h1 className="text-[28px] tracking-[-0.3px] leading-[1.3] text-white font-bold mb-4 md:text-[50px] md:leading-[1.1] md:tracking-[-0.5px]">
+        Learn to code by watching others
+      </h1>
+      <p className="text-base text-white leading-[1.65] font-medium">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+    </div>
+  );
+}
