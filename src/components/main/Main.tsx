065581cc36f44a93f3ccd5583df2c799daab6376
Demetre Berdzenadze <berdzena.demetre@gmail.com>
2026-09-04 21:29:06 +0400

    feat: add error handling and validation to Main component inputs

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,12 +5,11 @@ const Main = () => {
   const [isEmpty, setIsEmpty] = useState<number[]>([]);
   const [isFull, setIsFull] = useState<number[]>([]);
 
-  function foo() {
-    if (isFull.length === 0) {
-      for (let num of inputs) {
-        setIsEmpty((prev) => [...prev, num.id]);
+  function handleErrorInput(): void {
+    for (let data of inputs) {
+      if (!isFull.includes(data.id)) {
+        setIsEmpty((prev) => [...prev, data.id]);
       }
-      return;
     }
   }
 
@@ -24,6 +23,8 @@ const Main = () => {
     }
   }
 
+  console.log(isEmpty);
+
   return (
     <div className="flex flex-col">
       <button className="bg-trial text-white py-4.5 px-16.5 text-center rounded-[10px] max-w-118.5 text-[15px] font-medium leading-[1.73] tracking-[0.27px] hover:bg-trial-2  ">
@@ -33,27 +34,39 @@ const Main = () => {
       <div className="bg-white p-6 mt-6 max-w-118.5 rounded-[10px] lg:px-10 lg:py-8">
         <form
           className="flex flex-col gap-4"
-          onSubmit={(e) => e.preventDefault()}
+          onSubmit={(e) => {
+            for (let data of inputs) {
+              if (!isFull.includes(data.id)) {
+                e.preventDefault();
+              }
+            }
+          }}
         >
           {inputs.map((input) => (
-            <input
-              key={input.id}
-              type={input.type}
-              placeholder={input.txt}
-              className={
-                isEmpty.includes(input.id)
-                  ? "border-2 border-error p-4 rounded-[5px] placeholder:text-txt text-txt outline-error"
-                  : "border border-[#dedede] p-4 rounded-[5px] placeholder:text-txt text-txt"
-              }
-              onChange={(e) =>
-                handleInputValue(e.target.value.trim(), input.id)
-              }
-            />
+            <div key={input.id} className="flex flex-col">
+              <input
+                type={input.type}
+                placeholder={input.txt}
+                className={
+                  isEmpty.includes(input.id)
+                    ? "w-full border-2 border-error p-4 rounded-[5px] placeholder:text-txt text-txt outline-error bg-[url(/images/icon-error.svg)] bg-no-repeat bg-position-[right_1rem_center] "
+                    : "w-full border border-[#dedede] p-4 rounded-[5px] placeholder:text-txt text-txt"
+                }
+                onChange={(e) =>
+                  handleInputValue(e.target.value.trim(), input.id)
+                }
+              />
+              <span className="self-end text-[11px] font-medium text-error italic leading-[1.55] ">
+                {isEmpty.includes(input.id)
+                  ? `${input.txt} cannot be empty`
+                  : null}
+              </span>
+            </div>
           ))}
           <button
             type="submit"
             className="bg-submit text-[15px] text-white font-semibold py-4 tracking-[2px] leading-[1.75] hover:bg-light-submit rounded-[5px] "
-            onClick={() => foo()}
+            onClick={() => handleErrorInput()}
           >
             CLAIM YOUR FREE TRIAL
           </button>
